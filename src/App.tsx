48a5c4b53feaf00790451eec652acc2e856a610a
Carlos Ramirez <carlos@extrasistemas.com>
2023-02-13 20:14:27 -0600

    added function useEfect with example Math.min

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { Counter } from './bases/Counter';
 import { CounterBy } from './bases/CounterBy';
-
+import { CounterEfect } from './bases/CounterEfect';
 function App() {
   return (
     <>
       <Counter/>
      <CounterBy initialValue={0}/>
+     <CounterEfect/>
     </>
   );
 }
